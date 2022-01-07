7d484994e41bc8ac020c513294c74a1994546239
Danilo Costilla <danilo.01@live.com>
2022-01-06 19:55:36 -0600

    changed colors and added about text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,12 +7,19 @@ const About = () => {
       <div className="left">
         <h2>ABOUT ME</h2>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi aliquam
-          non reiciendis in omnis, dicta expedita. Eius, voluptates. Omnis,
-          consequatur atque. Officiis illum labore voluptates vitae, et facilis
-          soluta aperiam!
+          Hello! My name is Danilo Costilla (dah-knee-low kow-stee-yah) and I am
+          a fullstack software engineer. I got my education from Springboard's
+          online software engineer ocurse, see my certificate
+          <a href="https://www.credential.net/a06fbbc6-f1ac-4528-bfe0-1b70dc7d5524">
+            here
+          </a>
+          . I love to build things which is why my hobbies are programming,
+          music production, putting together LEGO's and gardeing. Feel free to
+          reach out if you would like to work together or just want to connect!
         </p>
-        <a href="#contact">Contact Me</a>
+        <a classname="button" href="#contact">
+          Contact Me
+        </a>
       </div>
       <div className="right">
         <div className="imageContainer">
